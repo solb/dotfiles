d9bba7e940388e00067b22545c8b5ff6d9a5436c
Sol Boucher <sboucher@cmu.edu>
2022-01-01 18:41:21 -0500

    .exports.sh: Add .deno/bin to the search path

--- .exports.sh
+++ .exports.sh
@@ -1,3 +1,3 @@
 export _RR_TRACE_DIR="."
 export EDITOR="vi"
-export PATH="$HOME/.cargo/bin:$PATH:/usr/local/sbin:/usr/sbin:/sbin"
+export PATH="$HOME/.cargo/bin:$HOME/.deno/bin:$PATH:/usr/local/sbin:/usr/sbin:/sbin"
